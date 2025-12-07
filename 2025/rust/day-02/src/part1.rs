aa671adf1ba2af259a2eb87142edeae49ee1bbbf
Ricardo Machado <rjfmachado@users.noreply.github.com>
2025-12-07 18:39:31 +0000

    Part 2 day 01

--- 2025/rust/day-02/src/part1.rs
+++ 2025/rust/day-02/src/part1.rs
@@ -1,23 +1,25 @@
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
-    let mut count = 0;
+    let mut sum: u64 = 0;
     let _range_list: Vec<_> = input
         .split(",")
         .map(|range| {
             dbg!(&range);
-            let _id_pair: Vec<_> = range
-                .split("-") // TODO: use split_once
-                .map(|id| {
-                    dbg!(id);
-                    // let mut invalid = false;
-                    // if &id[0..1] == "0" {
-                    //     invalid = true;
-                })
-                .collect();
+            let id_pair = range.split_once("-").unwrap();
+            for i in id_pair.0.parse::<u64>().unwrap()
+                ..=id_pair.1.parse::<u64>().unwrap()
+            {
+                let s = i.to_string();
+                let half = s.len() / 2;
+                if &s[..half] == &s[half..] {
+                    sum += s.parse::<u64>().unwrap();
+                    dbg!(s, sum);
+                }
+            }
         })
         .collect();
-
-    Ok(count.to_string())
+    dbg!(&sum);
+    Ok(sum.to_string())
 }
 
 #[cfg(test)]
@@ -26,9 +28,7 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
-1698522-1698528,446443-446449,38593856-38593862,565653-565659,
-824824821-824824827,2121212118-2121212124";
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
         assert_eq!("1227775554", process(input)?);
         Ok(())
     }
--- 2025/rust/day-02/src/part2.rs
+++ 2025/rust/day-02/src/part2.rs
@@ -1,6 +1,25 @@
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut sum: u64 = 0;
+    let _range_list: Vec<_> = input
+        .split(",")
+        .map(|range| {
+            dbg!(&range);
+            let id_pair = range.split_once("-").unwrap();
+            for i in id_pair.0.parse::<u64>().unwrap()
+                ..=id_pair.1.parse::<u64>().unwrap()
+            {
+                let s = i.to_string();
+                let half = s.len() / 2;
+                if &s[..half] == &s[half..] {
+                    sum += s.parse::<u64>().unwrap();
+                    dbg!(s, sum);
+                }
+            }
+        })
+        .collect();
+    dbg!(&sum);
+    Ok(sum.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +28,8 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        assert_eq!("1227775554", process(input)?);
         Ok(())
     }
 }
