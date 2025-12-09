e718c78d387486ec521129de2d7f6ca639192708
Ricardo Machado <rjfmachado@users.noreply.github.com>
2025-12-09 14:49:03 +0000

    day 2 part 2 done

--- 2025/rust/day-02/src/part2.rs
+++ 2025/rust/day-02/src/part2.rs
@@ -4,28 +4,43 @@ pub fn process(input: &str) -> miette::Result<String> {
     let _range_list: Vec<_> = input
         .split(",")
         .map(|range| {
-            dbg!(&range);
+            //dbg!(&range);
             let id_pair = range.split_once("-").unwrap();
-            for i in id_pair.0.parse::<u64>().unwrap()
+            for id in id_pair.0.parse::<u64>().unwrap()
                 ..=id_pair.1.parse::<u64>().unwrap()
             {
-                let s = i.to_string();
-                let half = s.len() / 2;
+                let id_string = id.to_string();
+                let id_len = id_string.len();
+                let mut invalid = false;
 
-                for j in 0..half {
-                    if s[half..].contains(&s[..j]) {
-                        sum += i;
-                        dbg!(&s, sum);
-                    }
+                for match_size in 1..id_len {
+                    let match_pattern =
+                        &id_string[0..match_size];
+
+                    let match_count = id_string
+                        .matches(match_pattern)
+                        .count();
+                    // dbg!(
+                    //     &id,
+                    //     match_count,
+                    //     match_size,
+                    //     match_pattern
+                    // );
+                    if match_count * match_size == id_len
+                        && match_count >= 2
+                    {
+                        invalid = true;
+                        break;
+                    };
                 }
-                // if &s[..half] == &s[half..] {
-                //     sum += s.parse::<u64>().unwrap();
-                //     dbg!(s, sum);
-                // }
+
+                if invalid {
+                    sum += id;
+                    //dbg!(id, sum);
+                }
             }
         })
         .collect();
-    dbg!(&sum);
     Ok(sum.to_string())
 }
 
@@ -35,9 +50,9 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        let input = "11-22,95-115,998-1012";
-        //let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
-        assert_eq!("1227775554", process(input)?);
+        //let input = "11-22,98-112,998-1010";
+        let input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
+        assert_eq!("4174379265", process(input)?);
         Ok(())
     }
 }
