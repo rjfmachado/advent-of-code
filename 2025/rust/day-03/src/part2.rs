f4869889eed96f2491afc387a43bef3fc511dae9
Ricardo Machado <rjfmachado@users.noreply.github.com>
2025-12-12 16:37:55 +0000

    day 03 part 2 done

--- 2025/rust/day-03/src/part2.rs
+++ 2025/rust/day-03/src/part2.rs
@@ -1,6 +1,34 @@
+use itertools::Itertools;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<String> {
+    const WORKING_BATTERY_SIZE: usize = 12;
+    let mut output_joltage: u64 = 0;
+    let mut working_battery_banks: Vec<String> = Vec::new();
+    let battery_banks: Vec<&str> = input.lines().collect();
+
+    for mut bank in battery_banks {
+        //dbg!(&bank);
+        let mut working_battery_bank = String::new();
+        for battery in (0..WORKING_BATTERY_SIZE).rev() {
+            //dbg!(&battery);
+            let max = &bank[0..&bank.len() - battery]
+                .chars()
+                .enumerate()
+                .max_set_by_key(|&(_index, joltage)| {
+                    joltage
+                })[0];
+            //dbg!(&max);
+            working_battery_bank.push(max.1);
+            bank = &bank[max.0 + 1..];
+        }
+        output_joltage +=
+            &working_battery_bank.parse::<u64>().unwrap();
+        working_battery_banks.push(working_battery_bank);
+    }
+    //dbg!(&working_battery_banks);
+
+    Ok(output_joltage.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +37,11 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "987654321111111
+811111111111119
+234234234234278
+818181911112111";
+        assert_eq!("3121910778619", process(input)?);
         Ok(())
     }
 }
