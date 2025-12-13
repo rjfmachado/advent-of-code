7ddf38d64fc47079972bb8cd6ab6b2645a77d65a
Ricardo Machado <rjfmachado@users.noreply.github.com>
2025-12-13 08:36:41 +0000

    wrong

--- 2025/rust/day-04/src/part1.rs
+++ 2025/rust/day-04/src/part1.rs
@@ -1,5 +1,3 @@
-use std::ops::Add;
-
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
     let mut accessible: u64 = 0;
@@ -12,7 +10,7 @@ pub fn process(input: &str) -> miette::Result<String> {
 
     for row in 0..grid.len() {
         for column in 0..grid[row].len() {
-            if get_surrounding(&grid, row, column, 2) < 4 {
+            if get_surrounding(&grid, row, column, 1) < 4 {
                 accessible += 1
             }
         }
@@ -30,11 +28,7 @@ fn get_surrounding(
     let mut surrounding: usize = 0;
     let row_count: usize = source.len();
     let column_count: usize = source[row].len();
-
     for x in -size..size {
-        let target_row =
-            (row as isize).checked_add(x).unwrap_or(0);
-
         if row as isize + x >= 0
             && row as isize + x <= row_count as isize
         {
