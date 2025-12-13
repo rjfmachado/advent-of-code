2dd26f00f085106b211f9c43bb4b4ddf430c6c75
Ricardo Machado <rjfmachado@users.noreply.github.com>
2025-12-13 08:33:51 +0000

    it's a mess

--- 2025/rust/day-04/src/part1.rs
+++ 2025/rust/day-04/src/part1.rs
@@ -1,6 +1,62 @@
+use std::ops::Add;
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut accessible: u64 = 0;
+    let grid: Vec<Vec<char>> = input
+        .lines()
+        .map(|line| line.chars().collect())
+        .collect();
+    //dbg!(&grid);
+    //dbg!(&grid[0][0]);
+
+    for row in 0..grid.len() {
+        for column in 0..grid[row].len() {
+            if get_surrounding(&grid, row, column, 2) < 4 {
+                accessible += 1
+            }
+        }
+    }
+
+    Ok(accessible.to_string())
+}
+
+fn get_surrounding(
+    source: &Vec<Vec<char>>,
+    row: usize,
+    column: usize,
+    size: isize,
+) -> u64 {
+    let mut surrounding: usize = 0;
+    let row_count: usize = source.len();
+    let column_count: usize = source[row].len();
+
+    for x in -size..size {
+        let target_row =
+            (row as isize).checked_add(x).unwrap_or(0);
+
+        if row as isize + x >= 0
+            && row as isize + x <= row_count as isize
+        {
+            for y in -size..size {
+                if column as isize + y >= 0
+                    && column as isize + y
+                        <= column_count as isize
+                {
+                    if source[row + x as usize]
+                        [column + y as usize]
+                        == '@'
+                        && x != 0
+                        && y != 0
+                    {
+                        surrounding += 1;
+                    }
+                }
+            }
+        }
+    }
+
+    surrounding as u64
 }
 
 #[cfg(test)]
@@ -9,9 +65,17 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.";
+        assert_eq!("13", process(input)?);
         Ok(())
     }
 }
