409dd891a6e519bb76a3d20f72c52a5f7cf4a819
Ricardo Machado <rjfmachado@users.noreply.github.com>
2025-12-15 13:37:10 +0000

    day 04 part 1

--- 2025/rust/day-04/src/part1.rs
+++ 2025/rust/day-04/src/part1.rs
@@ -1,17 +1,22 @@
+const ADJACENT: usize = 1;
+
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
-    let mut accessible: u64 = 0;
+    let mut accessible: usize = 0;
     let grid: Vec<Vec<char>> = input
         .lines()
         .map(|line| line.chars().collect())
         .collect();
-    //dbg!(&grid);
-    //dbg!(&grid[0][0]);
 
     for row in 0..grid.len() {
         for column in 0..grid[row].len() {
-            if get_surrounding(&grid, row, column, 1) < 4 {
-                accessible += 1
+            if grid[row][column] == '@' {
+                if get_surrounding(
+                    &grid, row, column, ADJACENT,
+                ) < 4
+                {
+                    accessible += 1
+                }
             }
         }
     }
@@ -23,34 +28,43 @@ fn get_surrounding(
     source: &Vec<Vec<char>>,
     row: usize,
     column: usize,
-    size: isize,
-) -> u64 {
+    size: usize,
+) -> usize {
     let mut surrounding: usize = 0;
     let row_count: usize = source.len();
-    let column_count: usize = source[row].len();
-    for x in -size..size {
-        if row as isize + x >= 0
-            && row as isize + x <= row_count as isize
-        {
-            for y in -size..size {
-                if column as isize + y >= 0
-                    && column as isize + y
-                        <= column_count as isize
+    let column_count: usize = source[0].len();
+
+    for x in -(size as isize)..=(size as isize) {
+        let target_row: Option<usize> =
+            row.checked_add_signed(x);
+
+        match target_row {
+            Some(row) => {
+                for y in -(size as isize)..=(size as isize)
                 {
-                    if source[row + x as usize]
-                        [column + y as usize]
-                        == '@'
-                        && x != 0
-                        && y != 0
-                    {
-                        surrounding += 1;
+                    let target_column: Option<usize> =
+                        column.checked_add_signed(y);
+
+                    match target_column {
+                        Some(column) => {
+                            if row < row_count
+                                && column < column_count
+                            {
+                                if source[row][column]
+                                    == '@'
+                                {
+                                    surrounding += 1;
+                                }
+                            }
+                        }
+                        None => {}
                     }
                 }
             }
+            None => {}
         }
     }
-
-    surrounding as u64
+    surrounding - 1
 }
 
 #[cfg(test)]
