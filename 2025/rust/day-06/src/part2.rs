c96f0ffc4867416aa7880496ceecd487b2ea8d3c
Ricardo Machado <rjfmachado@users.noreply.github.com>
2025-12-16 14:33:32 +0000

    day 6 part 2 wip

--- 2025/rust/day-06/src/part2.rs
+++ 2025/rust/day-06/src/part2.rs
@@ -1,6 +1,60 @@
+#[derive(Debug, Clone)]
+enum Operation {
+    Add,
+    Multiply,
+    None,
+}
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut data: Vec<_> = input.lines().collect();
+
+    let operations: Vec<Operation> = data
+        .last()
+        .unwrap()
+        .split(" ")
+        .filter(|operation| !operation.is_empty())
+        .map(|operation| match operation {
+            "*" => Operation::Multiply,
+            "+" => Operation::Add,
+            _ => Operation::None,
+        })
+        .collect();
+
+    data.truncate(&data.len() - 1);
+
+    let numbers: Vec<Vec<_>> = data
+        .into_iter()
+        .map(|number| number.chars().rev().collect())
+        .collect();
+
+    //dbg!(&operations);
+    dbg!(&numbers);
+
+    //let row_size: u64 = numbers[0].len() as u64;
+    //let col_size = numbers.len() as u64;
+
+    let mut result: u64 = 0;
+    // for x in 0..row_size as usize {
+    //     let mut column: u64 = 0;
+    //     for y in 0..(col_size) as usize {
+    //         //dbg!(&numbers[y][x]);
+    //         match &operations[x] {
+    //             Operation::Add => column += &numbers[y][x],
+    //             Operation::Multiply => {
+    //                 if column == 0 {
+    //                     column += &numbers[y][x]
+    //                 } else {
+    //                     column *= &numbers[y][x]
+    //                 }
+    //             }
+    //             Operation::None => {}
+    //         }
+    //     }
+    //     result += column;
+    // }
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +63,11 @@ mod tests {
 
     #[test_log::test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "123 328  51 64
+ 45 64  387 23
+  6 98  215 314
+*   +   *   +";
+        assert_eq!("4277556", process(input)?);
         Ok(())
     }
 }
