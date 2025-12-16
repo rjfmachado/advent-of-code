295fe68d3f551cc66cc7af9291cf0be5f05875c1
Ricardo Machado <rjfmachado@users.noreply.github.com>
2025-12-16 13:55:03 +0000

    day 06 part 1 done

--- 2025/rust/day-06/src/part1.rs
+++ 2025/rust/day-06/src/part1.rs
@@ -1,6 +1,66 @@
+#[derive(Debug, Clone)]
+enum Operation {
+    Add,
+    Multiply,
+    None,
+}
+
 #[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String> {
+    let mut data: Vec<_> = input.lines().collect();
+
+    let operations: Vec<Operation> = data
+        .last()
+        .unwrap()
+        .split(" ")
+        .filter(|operation| !operation.is_empty())
+        .map(|operation| match operation {
+            "*" => Operation::Multiply,
+            "+" => Operation::Add,
+            _ => Operation::None,
+        })
+        .collect();
+
+    data.truncate(&data.len() - 1);
+
+    let numbers: Vec<Vec<u64>> = data
+        .into_iter()
+        .map(|numbers| {
+            numbers
+                .split(" ")
+                .filter(|number| !number.is_empty())
+                .map(|n| n.parse::<u64>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    //dbg!(&operations);
+    //dbg!(&numbers);
+
+    let row_size: u64 = numbers[0].len() as u64;
+    let col_size = numbers.len() as u64;
+
+    let mut result: u64 = 0;
+    for x in 0..row_size as usize {
+        let mut column: u64 = 0;
+        for y in 0..(col_size) as usize {
+            //dbg!(&numbers[y][x]);
+            match &operations[x] {
+                Operation::Add => column += &numbers[y][x],
+                Operation::Multiply => {
+                    if column == 0 {
+                        column += &numbers[y][x]
+                    } else {
+                        column *= &numbers[y][x]
+                    }
+                }
+                Operation::None => {}
+            }
+        }
+        result += column;
+    }
+
+    Ok(result.to_string())
 }
 
 #[cfg(test)]
@@ -9,9 +69,11 @@ mod tests {
 
     #[test_log::test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "123 328  51 64
+ 45 64  387 23
+  6 98  215 314
+*   +   *   +";
+        assert_eq!("4277556", process(input)?);
         Ok(())
     }
 }
